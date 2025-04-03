4d8124eaa6ed0f8e0a9564ea7a4c502f6d21d020
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-04-03 15:33:07 +0200

    docs: document calling conventions

--- boot.s
+++ boot.s
@@ -11,7 +11,6 @@
 
 bits 16
 
-; Set up stack
 _setup:
   .prepare_stack:
      mov ax, 0x7C0 ; Data Segment
@@ -30,37 +29,38 @@ _setup:
 _main:
   call clear
 
-  push 0x0101
+  mov di, 0x0101
   call set_cursor
 
-  push text
+  mov di, text
   call println
 
   cli
   hlt
 
-; clear(void)
+; clear(void) -> void
+; Clears the screen and scrolls the window up.
 clear:
   fn_start
-
-  mov ax, 0x0700 ; Scroll Down and clear window
-  mov cx, 0x0000 ; Set top left corner in 0,0
-  mov dx, 0x184F ; Set bottom right corner in 18,4F
-  mov bh, 0x07   ; Set color 
-  int 0x10
-
+    mov ax, 0x0600 ; Scroll up and clear window
+    mov cx, 0x0000 ; Set top left corner in 0,0
+    mov dx, 0x184F ; Set bottom right corner in 18,4F
+    mov bh, 0x07   ; Set color for the background
+    int 0x10
   fn_end
 
-; set_cursor(di: col_row)
+; set_cursor(di: u16 col_row)
+; Sets the cursor position on the screen. Low byte is column, high byte is row.
 set_cursor:
   fn_start
-    mov dx, [bp+4] 
+    mov dx, di
     mov ah, 0x02
     mov bh, 0x00
     int 0x10
   fn_end
 
-; get_cursor(void) -> (ax: col_row)
+; get_cursor(void) -> (ax: u16 col_row)
+; Returns the current cursor position.
 get_cursor:
   fn_start
     mov ah, 0x03
@@ -69,31 +69,33 @@ get_cursor:
     mov ax, dx
   fn_end
 
-; put_char(u8 char)
+; put_char(di: u8 char) -> void
+; Prints a single character to the screen at the current cursor position.
 put_char:
   fn_start
-    mov ax, [bp+4] 
+    mov ax, di
     mov ah, 0x0A
     mov bh, 0x00
     mov cx, 1
     int 0x10
   fn_end
 
-; println(u8 *string)
+; println(di: u8 *string) -> void
+; Prints a null-terminated string to the screen, one character at a time.
 println:
   fn_start
-    mov si, [bp+4]
+    mov si, di ; Save the string pointer
 
   .loop:
     mov al, [si]
     cmp al, 0
     je .break
-    push ax
+    mov di, ax
     call put_char
     inc si
     call get_cursor
     add al, 1
-    push ax
+    mov di, ax
     call set_cursor
     jmp .loop
 
